ff7b13740e19f1c6b92a7d184e25d22ad77c45a2
Ezra Altahan <ei@users.noreply.github.com>
2017-01-08 04:06:02 -0800

    Create radio_dialog.hpp

---
+++ Test.Desert_E/0x141/radio_dialog.hpp
@@ -0,0 +1,20 @@
+class CfgRadio
+{
+    sounds[] = { radio_beep_to, radio_beep_from };
+
+
+    class radio_beep_to
+    {
+        name = "radio_beep_to";
+        sound[] = {"\0x141\sounds\radio_beep_to.ogg", db +0, 1.0};
+        title = "";
+    };
+    
+    class radio_beep_from
+    {
+        name = "radio_beep_from";
+        sound[] = {"\0x141\sounds\radio_beep_from.ogg", db +0, 1.0};
+        title = "";
+    };
+    
+};

f31742d9afc8e2894f5d6536219b3d4cdf2c5d81
Ezra Altahan <ei@users.noreply.github.com>
2017-01-08 04:06:33 -0800

    Deleted empty lines

--- Test.Desert_E/0x141/radio_dialog.hpp
+++ Test.Desert_E/0x141/radio_dialog.hpp
@@ -2,7 +2,6 @@ class CfgRadio
 {
     sounds[] = { radio_beep_to, radio_beep_from };
 
-
     class radio_beep_to
     {
         name = "radio_beep_to";
@@ -16,5 +15,4 @@ class CfgRadio
         sound[] = {"\0x141\sounds\radio_beep_from.ogg", db +0, 1.0};
         title = "";
     };
-    
 };

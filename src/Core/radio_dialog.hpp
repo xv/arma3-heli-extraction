4b32741f56396bc882bbaed75733a003c2d133e7
Jad Altahan <xviyy@aol.com>
2017-10-08 07:50:22 -0700

    Rename folders

--- src/Core/radio_dialog.hpp
+++ src/Core/radio_dialog.hpp
@@ -5,14 +5,14 @@ class CfgRadio
     class radio_beep_to
     {
         name = "radio_beep_to";
-        sound[] = {"\0x141\sounds\radio_beep_to.ogg", db +0, 1.0};
+        sound[] = {"\Core\sounds\radio_beep_to.ogg", db +0, 1.0};
         title = "";
     };
     
     class radio_beep_from
     {
         name = "radio_beep_from";
-        sound[] = {"\0x141\sounds\radio_beep_from.ogg", db +0, 1.0};
+        sound[] = {"\Core\sounds\radio_beep_from.ogg", db +0, 1.0};
         title = "";
     };
 };
